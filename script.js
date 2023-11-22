bb9a0ad985c083aaf3db1b5f24e47ed45f27740b
JohannD8 <146746845+JohannD8@users.noreply.github.com>
2023-11-22 16:28:20 +0100

    partie 2

--- script.js
+++ script.js
@@ -8,31 +8,38 @@ const caviste = document.getElementById("caviste")
 
 let e = typeCommerce
 
-function onChange() {
+let url = `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22${e.value}%22`
+
+function onChange(typeCommerce) {
     let value = e.value;
     let text = e.options[e.selectedIndex].text;
-    console.log(value, text);
-  }
-  e.onchange = onChange;
-  onChange();
+    //console.log(value, text);
+    fetch(url)
+    .then(response => response.json())
+    .then(response2 => console.log(response2))
+    }
 
-console.log(typeCommerce.value)
+  e.onchange = onChange;
+  onChange(typeCommerce);
+
+console.log("jeux = "+typeCommerce.value)
 
 
 //let choiceCommerce = fetch(`https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22${typeCommerce[0]}%22`)
 
-const choiceCommerce = async () => {
+/*const choiceCommerce = async () => {
     const typeCommerce = [jeux, librairie]
-    let commerce = await fetch (`https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22${e.value}%22`)
+    //let url = `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22${e.value}%22`
+    console.log("url : " + url)
+    let commerce = await fetch (url)
+
     console.log (commerce)
     let response = await commerce.json()
     console.log (response)
 
 
-}
+} */
 
 //fetch(`https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22Chocolaterie%20-%20P%C3%A2tisserie%22`)
 
 //chocolaterie.textContent = `#${response.id}`;
-
-//fetch(`https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22Chocolaterie%20-%20P%C3%A2tisserie%22`)
