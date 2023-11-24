588cb7f3c2026def74c4ca34498ba1e590abbaa7
JohannD8 <146746845+JohannD8@users.noreply.github.com>
2023-11-24 12:15:08 +0100

    part5

--- script.js
+++ script.js
@@ -6,53 +6,77 @@ const disquaire = document.getElementById("disquaire")
 const chocolaterie = document.getElementById("chocolaterie")
 const caviste = document.getElementById("caviste")
 
-//let e = typeCommerce
-
-//let url = `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22${typeCommerce.value}%22`
-
-let url = `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22Librairie%22`
-
-
 const onChange = async () => {
     let shops;
     let value = typeCommerce.value;
     console.log("valeur = "+value);
     let text = typeCommerce.options[typeCommerce.selectedIndex].text;
     console.log("texte =" +text);
+    let url = `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22${typeCommerce.value}%22`
+
     console.log(url)
     
     await fetch(url)
     .then(response => response.json())
     .then(response2 => {
-       shops = response2;
+       shops = response2.results;
         console.log("test", shops)
+
+        //document.getElementById("adresses").innerHTML = 
+        shops.map((shop) => {
+            //console.log(shop.adresse)
+
+            document.getElementById("nom_du_commerce").innerHTML +=shop.nom_du_commerce
+            document.getElementById("adresses").innerHTML +=shop.adresse
+            document.getElementById("code_postal").innerHTML +=shop.code_postal
+            document.getElementById("site_internet").innerHTML +=shop.site_internet
+            document.getElementById("telephone").innerHTML +=shop.telephone
+        //<div id=${results.adresse}>
+        //<div/>
+         }
+        )
     })
 }
 
-console.log(onChange());
+const newSelect = async () => {
+            document.getElementById("nom_du_commerce").innerHTML = ""
+            document.getElementById("adresses").innerHTML =""
+            document.getElementById("code_postal").innerHTML =""
+            document.getElementById("site_internet").innerHTML =""
+            document.getElementById("telephone").innerHTML =""
 
-console.log("test 2", shops)
+    let value = typeCommerce.value;
+        console.log("valeur = "+value);
+        let text = typeCommerce.options[typeCommerce.selectedIndex].text;
+        console.log("texte =" +text);
+        let url = `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22${typeCommerce.value}%22`
 
-//e.onchange = onChange;
+    console.log(url)
+    
+    await fetch(url)
+    .then(response => response.json())
+    .then(response2 => {
+       shops = response2.results;
+        console.log("test", shops)
 
-//console.log("jeux = "+typeCommerce.value)
+        //document.getElementById("adresses").innerHTML = 
+        shops.map((shop) => {
+            //console.log(shop.adresse)
+
+            document.getElementById("nom_du_commerce").innerHTML +=shop.nom_du_commerce
+            document.getElementById("adresses").innerHTML +=shop.adresse
+            document.getElementById("code_postal").innerHTML +=shop.code_postal
+            document.getElementById("site_internet").innerHTML +=shop.site_internet
+            document.getElementById("telephone").innerHTML +=shop.telephone
+        //<div id=${results.adresse}>
+        //<div/>
+         }
+        )
+    })
+}
+
+onChange();
 
 
-//let choiceCommerce = fetch(`https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22${typeCommerce[0]}%22`)
-
-/*const choiceCommerce = async () => {
-    const typeCommerce = [jeux, librairie]
-    //let url = `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22${e.value}%22`
-    console.log("url : " + url)
-    let commerce = await fetch (url)
-
-    console.log (commerce)
-    let response = await commerce.json()
-    console.log (response)
 
 
-} */
-
-//fetch(`https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=20&refine=type_de_commerce%3A%22Chocolaterie%20-%20P%C3%A2tisserie%22`)
-
-//chocolaterie.textContent = `#${response.id}`;
